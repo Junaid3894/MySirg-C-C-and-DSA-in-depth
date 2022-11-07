15c77e3c92f9225fde8b98e63a52f08f24bb5ead
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-07 19:40:58 +0530

    Create Answer8.c

---
+++ Assignment 1.c/Answer8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int number; // Declearetion of variable
+printf("Enter the number which you want to find its square:"); // Taking input from user 
+scanf("%d",&number);
+printf("Square of %d is %d",number,number*number); // prints the square of number 
+
+
+return 0;
+}
