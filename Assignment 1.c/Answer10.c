db9a0bd76d61b59dbeadc6d0ee6857d1703f1ef3
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-07 21:08:36 +0530

    Create Answer10.c

---
+++ Assignment 1.c/Answer10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float radius;
+printf("Enter the radius of circle :");
+scanf("%f",&radius);
+printf("Area of circle is %.2f having radius %.2f",3.14*radius*radius,radius);
+
+return 0;
+}
