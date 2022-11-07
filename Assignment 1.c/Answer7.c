498e8afa67cd8a3e601ebd6f33fdb6b50cf12d42
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-07 19:34:59 +0530

    Create Answer7.c

---
+++ Assignment 1.c/Answer7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+int sum; // Summation of a and b
+printf("Enter the value of a and b :"); // Taking input from user 
+scanf("%d %d",&a,&b);
+sum=a+b;
+printf("Sum of %d and %d is %d",a,b,sum); // Finally printing the sum of a and b
+
+
+return 0;
+}
