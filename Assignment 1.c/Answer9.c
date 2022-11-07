d88fa4e346f03a3ba90d8cdce43c67bde7e06289
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-07 21:01:42 +0530

    Create Answer9.c

---
+++ Assignment 1.c/Answer9.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int length; 
+int width;
+printf("Enter the length and width of rectangle :");
+scanf("%d %d",&length,&width);
+printf("Area of rectangle is %d",length*width);
+return 0;
+}
