2be9880c18e099051b65785fdab54e46ed7bfa3a
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-07 21:19:45 +0530

    Create Answer1.c

---
+++ Assignment 2.c/Answer1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float a,b,c;
+printf("Enter the value of a b and c :");
+scanf("%f%f%f",&a,&b,&c);
+printf("Average of a b and c is %.2f",(a+b+c)/3);
+
+return 0;
+}
