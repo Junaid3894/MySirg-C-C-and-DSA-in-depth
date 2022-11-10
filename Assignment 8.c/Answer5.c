60e04d8256a2a3cc5148c8eb97f8c4aba65fa467
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-10 23:14:04 +0530

    Update Answer5.c

--- Assignment 8.c/Answer5.c
+++ Assignment 8.c/Answer5.c
@@ -5,7 +5,7 @@ int n;
 printf("Enter how many first odd numbers you want to see in reverse order:");
 scanf("%d",&n);
 for(int i=1;i<=n;i++){
-    printf("%d\n",21-2*i);
+    printf("%d\n",n*2-(2*i-1));
 }
 return 0;
 }
