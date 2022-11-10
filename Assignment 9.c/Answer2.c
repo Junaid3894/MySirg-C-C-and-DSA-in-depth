0dead626a27c5d07591cd30184a1d040dfc7a00b
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-10 23:44:49 +0530

    Create Answer2.c

---
+++ Assignment 9.c/Answer2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,sum=0;;
+printf("Enter how many first even numbers you want add :");
+scanf("%d",&n);
+for(int i=1;i<=n;i++){
+    if(i%2==0)
+        sum+=i;
+}
+printf("Sum of first %d numbers are %d",n,sum);    
+return 0;
+}
