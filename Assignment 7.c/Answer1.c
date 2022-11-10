19b3480ca7f11bce5b1cd70de9fa5cc67a461ea0
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-10 14:36:40 +0530

    Create Answer1.c

---
+++ Assignment 7.c/Answer1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i;
+// Start loop counter from 1 and go till i<=5
+// Increment the loop by 1 in each iteration
+// Each repetition of loop print Mysirg string
+for(i=1;i<=5;i++){
+    printf("MySirg\n");
+}
+return 0;
+}
