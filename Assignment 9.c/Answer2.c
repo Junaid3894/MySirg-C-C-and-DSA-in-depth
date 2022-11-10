baa479e79a8840bf6a17faced98be565528fab0c
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-11 00:06:32 +0530

    Update Answer2.c

--- Assignment 9.c/Answer2.c
+++ Assignment 9.c/Answer2.c
@@ -4,7 +4,7 @@ int main()
 int n,sum=0;;
 printf("Enter how many first even numbers you want add :");
 scanf("%d",&n);
-for(int i=1;i<=n;i++){
+for(int i=1;i<=n*2;i++){
     if(i%2==0)
         sum+=i;
 }
