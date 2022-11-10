b4dfad8909a49acd82f270e67bdf2b6fe6089660
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-10 14:16:24 +0530

    Create Answer7.c

---
+++ Assignment 2.c/Answer7.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int ASCII;
+printf("Enter the ASCII to see its character :");
+scanf("%d",&ASCII);
+printf("Your ASCII %d of character is %c",ASCII,ASCII);
+return 0;
+}
