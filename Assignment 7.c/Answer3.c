f1d8feafe79041dd8734c70b1f56a0ab122dea16
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-10 14:49:02 +0530

    Create Answer3.c

---
+++ Assignment 7.c/Answer3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i;
+// Start loop counter from 10 and go till i>=1
+// decrement the loop by 1 in each iteration
+// Each repetition of loop print i value 
+for(i=10;i>=1;i--){
+    printf("%d\n",i);
+}
+return 0;
+}
