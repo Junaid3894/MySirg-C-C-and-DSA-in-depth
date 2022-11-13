2e39ee38e5dd215a3a080ebf21bb4a46ffabd293
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-13 10:15:58 +0530

    Update Answer2.c

--- Assignment 10.c/Answer2.c
+++ Assignment 10.c/Answer2.c
@@ -4,7 +4,7 @@ int main()
 int n;
 int a=0,b=1;
 int c;
-printf("Enter the nth term of fibonacci as far as you want to see :");
+printf("Enter the n term of fibonacci as far as you want to see :");
 scanf("%d",&n);
 printf("%d\t%d\t",a,b);
 for(int i=1;i<=n-2;i++)
