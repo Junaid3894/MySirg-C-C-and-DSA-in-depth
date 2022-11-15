5c01c272569c6ffffc37a631177be67ce6109218
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-16 00:37:53 +0530

    Create Answer3.c

---
+++ Assignment 4.c/Answer3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    char c;
+    double d;
+    printf("Size of int is %d\nSize of float is \nsize of char is %d\nsize of double is %d",sizeof(a),sizeof(b),sizeof(c),sizeof(d));
+    return 0;
+}
