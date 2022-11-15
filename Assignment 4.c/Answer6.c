415c04c278835305a6dc3d603e6bc99179c2f248
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-16 00:59:35 +0530

    Create Answer6.c

---
+++ Assignment 4.c/Answer6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int INR;
+    float USD;
+    printf("Enter INR :");
+    scanf("%d",&INR);
+    USD=INR / 84.23;
+    printf("After converted Your USD is %f",USD);
+
+    return 0;
+}
