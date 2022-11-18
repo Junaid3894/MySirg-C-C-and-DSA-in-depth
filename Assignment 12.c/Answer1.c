489ddf367b4ea930845498d05a1f7ce0a8ccb4ec
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-18 21:38:49 +0530

    Update Answer1.c

--- Assignment 12.c/Answer1.c
+++ Assignment 12.c/Answer1.c
@@ -7,7 +7,7 @@ int main()
     scanf("%d",&rows);
     for(i=1;i<=rows;i++)
     {
-        for(j=1;j<=rows*rows-1;j++){
+        for(j=1;j<=rows+rows-1;j++){
             if(j>=rows+1-i && j<=rows-1+i)
             {
                 printf(" * ");
