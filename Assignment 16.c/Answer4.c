8358705bdbbca3f0664bc37bdd004145bb33fef4
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-11-27 13:22:39 +0530

    Create Answer4.c

---
+++ Assignment 16.c/Answer4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int odd_printN(int n );
+int main()
+{
+    int n;
+    printf("Enter n as far you want to see :");
+    scanf("%d",&n);
+    odd_printN(n*2);   
+    return 0;
+}
+int  odd_printN(int n )
+{
+    if(n==0)
+        return 1;
+    else
+    {
+        if(n%2)
+            printf("%d\t",n);
+        odd_printN(n-1);
+    }    
+} 
