38977d23b8601ecfc6895b38fc6cecc75bf8bb33
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-12-04 18:38:41 +0530

    Create Answer10.c

---
+++ Assignment 12.c/Answer10.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    int k;
+    for(i=1;i<=4;i++)
+    {
+        k=64;
+        for(j=1;j<=7;j++)
+        {
+            j>4?k--:k++;
+            if(j<=5-i || j>=3+i)
+                printf(" %c ",k);
+            else
+                printf("   ");    
+        }
+        printf("\n");
+    }   
+    return 0;
+}
