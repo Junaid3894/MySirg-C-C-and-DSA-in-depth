f898ba20a2431570400f1ebc8f9faa7d0459184c
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2022-12-14 14:34:56 +0530

    Create Answer7.c

---
+++ Assignment 19.c/Answer7.c
@@ -0,0 +1,43 @@
+/* Write a function to count a total number of dublicate elements in an array
+(means elements that occurs two times in an array)*/
+#include<stdio.h>
+void occurence(int *arr,int size);
+int main()
+{
+   int arr[7];
+   // Taking input from user
+   for(int i=0;i<7;i++)
+   {
+      printf("Enter the %d value of an array :",i+1);
+      scanf("%d",&arr[i]);
+   }
+   occurence(arr,7);
+   return 0;
+}
+void occurence(int *arr,int size)
+{
+    int arr_count[size];
+    arr_count[0]=0;
+    int count=1;
+   for(int i=0;i<size;i++)
+   {
+    for(int j=i+1;j<size;j++)
+    {
+        if(arr[i]==arr[j])
+        {
+            count++;
+            arr[j]=0;
+        }
+
+    }
+    arr_count[i]=count;
+    count=1;
+   }
+printf("Dublicate elements are.........\n");   
+for(int i=0;i<size;i++)
+{
+    if(arr[i]!=0)
+        if(arr_count[i]>1)
+            printf("%d\t",arr[i]);
+}
+}
