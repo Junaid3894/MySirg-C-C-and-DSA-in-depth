0b64820cf13ed7f5886d5ba3a48e152a3b10fe40
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2023-01-05 21:13:41 +0530

    Create Answer5.c

---
+++ Assignment 21 String basics.c/Answer5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   char str[20];
+   printf("Enter the string :");
+   fgets(str,20,stdin);
+   str[strlen(str)-1]='\0';
+   for(int i=0;str[i];i++)
+   {
+    if(str[i]!=32)
+        str[i]-=32;
+   }
+   printf("Your string after uppercase..\n");
+   printf("%s",str);
+   return 0;
+}
