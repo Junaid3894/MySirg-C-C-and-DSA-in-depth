ae6d7ca73761fb442d6042660b065055a00a57a3
Mohd Junaid <109454576+Junaid3894@users.noreply.github.com>
2023-01-14 01:31:38 +0530

    Create Answer7.c

---
+++ Assignment 16.c/Answer7.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void square_print(int n );
+int main()
+{
+    int n;
+    printf("Enter n as far you want to see :");
+    scanf("%d",&n);
+    square_print(n);   
+    return 0;
+}
+void square_print(int n )
+{
+    if(n>0)
+    {
+        square_print(n-1);
+        printf("%d\n",n*n);
+    }    
+} 
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
